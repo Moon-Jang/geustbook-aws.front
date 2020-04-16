50acac4d4ed1b0bd2f9fe036eb0c2fb53ee2dea9
guestbook <ubuntu@ip-172-31-44-33.ap-northeast-2.compute.internal>
2020-04-16 11:03:27 +0000

    0416

--- guestbook/src/App.js
+++ guestbook/src/App.js
@@ -1,15 +1,26 @@
 import React from 'react';
-//import logo from './logo.svg';
 import './App.css';
+import classNames from 'classnames/bind';
+import styles from './App.css';
+import logo from './logo.svg';
 
- function App() {
+const st = classNames.bind(styles);
+function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-      </header>
+    <div id="App">
+      <div id="background"></div>
+      <div id="wrapper">
+        <div id ="logo">
+          <img src={logo} id="logo" alt="error"/>
+        </div>
+        <div className="container">
+          <div className="row">
+            <div className={st('col-md-2','space-area')}></div>
+            <div className={st('col-md-8','context')}></div>
+            <div className={st('col-md-2','space-area')}></div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 } 
---
+++ guestbook/src/Board.js
@@ -0,0 +1,16 @@
+import React from 'react';
+class Board extends React.Component {
+  /* renderSquare(i) {
+    return (
+      <Square
+        value={this.props.squares[i]}
+        onClick={() => this.props.onClick(i)}
+      />
+    );
+  } */
+  render() {
+    return (
+      null
+      );
+  }
+}
